86c2fb1ac9813aa18a955d903d151594dffc612e
Robert Bubnik <rbubnik@gmail.com>
2023-07-29 18:58:19 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const { arrayContaining } = require("expect");
+
+  function findMatching(arrayOfDrivers, searchName) {
+   const names = arrayOfDrivers.filter(name => name.toLowerCase()=== searchName.toLowerCase())
+console.log(arrayOfDrivers);
+   return names;
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+  function fuzzyMatch(arrayOfDrivers, searchTerm) {
+    const names = arrayOfDrivers.filter(orb => orb.startsWith(searchTerm))
+    
+    return names;
+  }
+ 
+ function matchName(arrayOfDrivers, name) {
+    const names = arrayOfDrivers.filter(orb => orb.name === name)
+    return names;
+ }
+
